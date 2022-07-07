2e321e9ea75224e95339336097e140c3ec130c01
justin.bunag <justin.bunag@pointwest.com.ph>
2022-07-07 16:41:32 +0800

    absolute and relative positioning

--- App.js
+++ App.js
@@ -40,7 +40,7 @@ export default function App() {
            <View
             style={{
               backgroundColor: "dodgerblue",
-              flexBasis: 100, // can map to width or height
+              width: 100, // can map to width or height
               height: 100
             }} 
           /> 
@@ -48,7 +48,10 @@ export default function App() {
             style={{
               backgroundColor: "gold",
               width: 100, 
-              height: 100
+              height: 100, 
+              top: 20,
+              left: 20,
+              position: "absolute"
             }} 
           />
           <View
