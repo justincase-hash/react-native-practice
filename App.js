024366ab4cf08f0cfdda610019470f4ec3958399
justin.bunag <justin.bunag@pointwest.com.ph>
2022-07-11 10:11:39 +0800

    viewImage Screen & colors.js

--- App.js
+++ App.js
@@ -14,64 +14,11 @@ import {
  
 import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {   
   return (
-    <WelcomeScreen />
+    <ViewImageScreen/>
   );      
 }
 
-
-/* 
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-    alignItems: "center",
-    paddingTop : Platform.OS === "android" ? StatusBar.currentHeight : 0
-  },
-}); 
- <View
-        style={{
-        backgroundColor: "dodgerblue",
-        width: "100%",
-        height: landscape ? "100%" : "30%",
-      }}></View>
-      <Text numberOfLines={1} onPress={handlePress}> Hello React Native. A really long text. I wanna make this really really long text
-      Hello React Native. A really long text. I wanna make this really really long text
-      </Text> 
-
-      <Button
-        title="Click Me"
-        onPress={() =>
-          Alert.alert("My Title", "My Message", [
-            { text: "Yes", onPress: () => console.log("yes") },
-            { text: "No", onPress: () => console.log("no")}
-          ])
-        }    
-      /> 
-
-      <Button
-        title="Click Me"
-        onPress={() =>
-          Alert.prompt("My Title", "My Message", text => console.log(text))
-        }    
-      />
-
-      <TouchableWithoutFeedback onPress={ () => console.log("test touchable") }>
-        <Image 
-          blurRadius={10}
-          fadeDuration={1000}
-          source={{
-            width: 200,
-            height: 300,
-            uri : "https://picsum.photos/200/300"
-          }}
-      />
-      </TouchableWithoutFeedback> 
-
-
-*/
--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,10 +1,45 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
+import colors from '../config/colors';
 
 function ViewImageScreen(props) {
     return (
-        <Image source={require('../assets/chair.jpg') } />
+        <View style={styles.container}>
+        <View style={styles.closeIcon}></View>
+        <View style={styles.deleteIcon}></View>
+        <Image
+            resizeMode="contain" //fitting the size of the image to the screen w/o crop.
+            style={styles.image} source={require('../assets/chair.jpg')} 
+        />
+        </View>
     );
 }
 
+
+const styles = StyleSheet.create({
+    closeIcon: {
+        width: 50,
+        height: 50,
+        backgroundColor: colors.primary,
+        position: "absolute",
+        top: 40,
+        left: 30
+    },
+    container: {
+        backgroundColor: colors.black,
+        flex: 1
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    },
+    deleteIcon: {
+        width: 50,
+        height: 50,
+        backgroundColor: colors.secondary,
+        position: "absolute",
+        top: 40,
+        left: 30
+    },
+})
 export default ViewImageScreen;
