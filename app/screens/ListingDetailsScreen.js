2a9617941d609a6ac8427cdc9e8fa6f460548b4b
justin.bunag <justin.bunag@pointwest.com.ph>
2022-07-25 11:38:42 +0800

    Listing Details Screen Component Fixed error

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         fontSize: 20,
         marginVertical: 10
-    }
+    },
     title: {
         fontSize: 24,
         fontWeight: "500"
