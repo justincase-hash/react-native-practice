d0e8ee92c877fffbbdcd602e029bf007f3a1a8e0
justin.bunag <justin.bunag@pointwest.com.ph>
2022-07-25 11:38:09 +0800

    Listing Details Screen

--- App.js
+++ App.js
@@ -20,17 +20,8 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import AppButton from './app/components/AppButton';
 import Card from './app/components/Card'
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 export default function App() {   
-  return <View style={{
-    backgroundColor: ' #f8f4f4',
-    padding: 20,
-    paddingTop: 100,
-  }}>
-    <Card 
-      title="Red Jacket for sale"
-      subTitle="$100"
-      image={require("./app/assets/chair.jpg")}
-      />
-  </View>     
+  return <ListingDetailsScreen/>   
 }
 
