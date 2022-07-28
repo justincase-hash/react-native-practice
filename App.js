26c14793d0c03d24ff94800a0fa684206e4d0bb1
justin.bunag <justin.bunag@pointwest.com.ph>
2022-07-28 15:34:51 +0800

    View Image screen improvement

--- App.js
+++ App.js
@@ -22,6 +22,6 @@ import AppButton from './app/components/AppButton';
 import Card from './app/components/Card'
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 export default function App() {   
-  return <ListingDetailsScreen/>   
+  return <ViewImageScreen/>   
 }
 
--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Image, StyleSheet, View } from 'react-native';
 import colors from '../config/colors';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 function ViewImageScreen(props) {
     return (
         <View style={styles.container}>
-        <View style={styles.closeIcon}></View>
-        <View style={styles.deleteIcon}></View>
+            <View style={styles.closeIcon}>
+                <MaterialCommunityIcons name="close" color="white" size={30} />    
+            </View>
+            <View style={styles.deleteIcon}>
+                <MaterialCommunityIcons name="trash-can-outline" color="white" size={30} />  
+            </View>
         <Image
             resizeMode="contain" //fitting the size of the image to the screen w/o crop.
             style={styles.image} source={require('../assets/chair.jpg')} 
@@ -18,9 +23,6 @@ function ViewImageScreen(props) {
 
 const styles = StyleSheet.create({
     closeIcon: {
-        width: 50,
-        height: 50,
-        backgroundColor: colors.primary,
         position: "absolute",
         top: 40,
         left: 30
@@ -34,9 +36,6 @@ const styles = StyleSheet.create({
         height: '100%'
     },
     deleteIcon: {
-        width: 50,
-        height: 50,
-        backgroundColor: colors.secondary,
         position: "absolute",
         top: 40,
         left: 30
