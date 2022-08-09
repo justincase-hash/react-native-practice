13a5839a7e9855fb8df0284f1b6993f3733634a7
justin.bunag <justin.bunag@pointwest.com.ph>
2022-08-09 13:21:00 +0800

    deleting an item & pull to refresh

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FlatList, StyleSheet } from 'react-native';
 import ListItem from '../components/ListItem';
 import ListItemDeleteAction from '../components/ListItemDeleteAction';
 import ListItemSeparator from '../components/ListItemSeparator';
 import Screen from '../components/Screen';
 
-const messages = [
+const initialMessages = [
     {
         id: 1,
         title: 'T1',
@@ -21,6 +21,14 @@ const messages = [
 ]
 
 function MessagesScreen(props) {
+    const [messages, setMessages] = useState(initialMessages); 
+    const [refreshing, setRefreshing] = useState(false)
+    
+    const handleDelete = message => {
+        const newMessages = setMessages(messages.filter(m => m.id !== message.id));
+       
+    }
+
     return (
         <Screen>
         <FlatList
@@ -32,9 +40,20 @@ function MessagesScreen(props) {
                     subTitle={item.description}
                     image={item.image} 
                     onPress={() => console.log("message selected", item)}
-                    renderRightActions={ListItemDeleteAction}
+                    renderRightActions={ () => <ListItemDeleteAction onPress={() => handleDelete(item)}/>}
                     />} 
-            ItemSeparatorComponent={ListItemSeparator}    
+                    ItemSeparatorComponent={ListItemSeparator}    
+                    refreshing={refreshing}
+                    onRefresh={() => {
+                        setMessages([
+                            {
+                                id: 2,
+                                title: 'T2',
+                                description: 'D2',
+                                image: require('../assets/chair.jpg')
+                            }
+                        ])
+                    }}
                     />    
         </Screen>
     );
